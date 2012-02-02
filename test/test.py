efe7c86fa8843fe8c0be32b2da05a995aee75a57
Tresia Burger <tresiab@miranetworks.net>
2012-02-02 15:04:41 +0200

    wip

--- test/test.py
+++ test/test.py
@@ -1,17 +1,26 @@
 #!/usr/bin/env python
 
-import sys, unittest
+import sys, unittest, junitxml
 
 def main(argv=sys.argv):
     import test_fibonacci
 
-    suite_test_fibonacci = unittest.makeSuite(test_fibonacci.TestFibonacci)
+   # suite_test_fibonacci = unittest.makeSuite(test_fibonacci.TestFibonacci)
 
-    alltests = unittest.TestSuite((
-        suite_test_fibonacci,
-    ))
+   # alltests = unittest.TestSuite((
+   #     suite_test_fibonacci,
+   # ))
+   # 
+   # unittest.TextTestRunner(verbosity=2).run(alltests)
 
-    unittest.TextTestRunner(verbosity=2).run(alltests)
+
+    with open('output/test_fibonacci.xml', 'w') as test_output:
+        result = junitxml.JUnitXmlResult(test_output)
+        result.startTestRun()
+        suite = unittest.TestLoader().loadTestsFromTestCase(test_fibonacci.TestFibonacci)
+        suite.run(result)
+        result.stopTestRun()
+
 
 #=============================================================================
 
